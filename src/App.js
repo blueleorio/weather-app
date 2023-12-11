233a420df632527b6eb73a34536ed83462d75c56
BeluKotu <vietthanh456@gmail.com>
2023-12-11 10:49:26 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+const api = {
+  key: "7363ac3eadc93d1e49f6e4eeed00ae03",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 function App() {
-  return <div>App</div>;
+  const [searchInput, setSearchInput] = useState("");
+  const [searchCity, setSearchCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState("");
+
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchCity(searchInput);
+  };
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      if (!searchCity) return;
+      setLoading(true);
+      // Process
+      try {
+        const url = `${api.base}weather?q=${searchCity}&units=metric&APPID=${api.key}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        if (res.ok) {
+          setWeatherInfo(
+            `${data.name}. ${data.sys.country} ${data.weather[0].description}, ${data.main.temp}`
+          );
+          setErrorMessage("");
+        } else {
+          setErrorMessage(data.message);
+        }
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+      setLoading(false);
+    };
+    fetchWeatherData();
+  }, [searchCity]);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="City"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <div>{weatherInfo}</div>
+          <div style={{ color: "red" }}> {errorMessage}</div>
+        </>
+      )}
+    </>
+  );
 }
 
 export default App;
